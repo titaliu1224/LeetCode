f182698f41b357b0a8e9e3a187d3d984c4cbce19
tita <71343865+titaliu1224@users.noreply.github.com>
2023-12-22 15:10:59 +0800

    edit 1422

---
+++ easy/1422.Maximum-Score-After-Splitting-a-String-edited.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxScore(self, s: str) -> int:
+        right = 0
+        for i in s:
+            right += int(i)
+
+        max_result = 0
+        left = 0
+        for i in range(0, len(s) - 1):
+            if s[i] == '0':
+                left += 1
+            else:
+                right -= 1
+
+            max_result = max(left + right, max_result)
+
+        return max_result
