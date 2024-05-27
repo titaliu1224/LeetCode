457f8e5f5cee68ac460a4575c8afc8534a92d7cb
tita <tiffanyliu1224@gmail.com.tw>
2024-05-28 00:32:39 +0800

    paste the correct code

--- easy/1608.special-array-with-x-elements-greater-than-or-equal-x.py
+++ easy/1608.special-array-with-x-elements-greater-than-or-equal-x.py
@@ -8,7 +8,7 @@
 class Solution:
     def specialArray(self, nums: List[int]) -> int:
         count = 0
-        for i in range(0, len(nums)):
+        for i in range(0, len(nums) + 1):
             count = 0
             for num in nums:
                 if num >= i:
